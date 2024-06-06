4e3c2f0e072c9b8efa6cf96233d608109fe47f15
doancarlosembara <carlosdoanembara@gmail.com>
2024-06-06 13:50:39 +0700

    salmon

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      ziggy: path.resolve(__dirname, 'vendor/tightenco/ziggy/dist/index.esm.js'),
+      'ziggy-js': path.resolve(__dirname, 'vendor/tightenco/ziggy/dist/index.esm.js'),
     },
   },
   build: {
