c3f34e41ebd7c7ac2ff38aca596515ea831ade7a
doancarlosembara <carlosdoanembara@gmail.com>
2024-12-02 20:15:30 +0700

    test

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import TextInput from '@/Components/TextInput.vue';
-import {Head, Link, useForm} from '@inertiajs/vue3';
-import {onMounted, ref} from "vue";
+import Checkbox from "@/Components/Checkbox.vue";
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
+import { onMounted, ref } from "vue";
 
 defineProps({
     canResetPassword: {
@@ -17,34 +17,33 @@ defineProps({
 });
 
 onMounted(() => {
-    document.documentElement.classList.remove('dark');
+    document.documentElement.classList.remove("dark");
 });
 
 const form = useForm({
-    email: ref('super@root.com'),
-    password: ref('password'),
+    email: ref("super@root.com"),
+    password: ref("password"),
     remember: false,
 });
 
 const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
+    form.post(route("login"), {
+        onFinish: () => form.reset("password"),
     });
 };
 </script>
 
 <template>
     <GuestLayout>
-        <Head title="Log in"/>
+        <Head title="Log in" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+        <div
+            v-if="status"
+            class="mb-4 font-medium text-sm text-green-600"
+        ></div>
 
         <form @submit.prevent="submit">
-            <div class="flex justify-center font-bold py-4">
-                Login
-            </div>
+            <div class="flex justify-center font-bold py-4">Login</div>
             <!-- 
             <div class="flex flex-col justify-center mb-4 text-sm">
                 <p class="mb-2">Admin -> email: '<b>super@root.com</b>' Password: '<b>password</b>' </p>
@@ -52,7 +51,7 @@ const submit = () => {
             </div>
              -->
             <div>
-                <InputLabel for="email" value="Email"/>
+                <InputLabel for="email" value="Email" />
 
                 <TextInput
                     id="email"
@@ -64,7 +63,7 @@ const submit = () => {
                     autocomplete="username"
                 />
 
-                <InputError class="mt-2" :message="form.errors.email"/>
+                <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
@@ -79,12 +78,12 @@ const submit = () => {
                     autocomplete="current-password"
                 />
 
-                <InputError class="mt-2" :message="form.errors.password"/>
+                <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
             <div class="block mt-4">
                 <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember"/>
+                    <Checkbox name="remember" v-model:checked="form.remember" />
                     <span class="ml-2 text-sm text-gray-600">Remember me</span>
                 </label>
             </div>
@@ -100,11 +99,11 @@ const submit = () => {
 
                 <button
                     class="ml-4 inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
-                    :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
+                    :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing"
                 >
                     Log in
                 </button>
-
             </div>
         </form>
     </GuestLayout>
